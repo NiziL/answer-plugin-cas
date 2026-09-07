15ea04ebb622a581d216c2940047a8052e63aa37
Théo BL <theo.biasutto-lervat@inria.fr>
2026-09-07 11:14:58 +0200

    feat: new display name for login button + doc

--- i18n/translation.go
+++ i18n/translation.go
@@ -1,8 +1,10 @@
 package i18n
 
 const (
-	InfoName                   = "plugin.cas_connector.backend.info.name"
-	InfoDescription            = "plugin.cas_connector.backend.info.description"
-	ConfigServerURLTitle       = "plugin.cas_connector.backend.config.server_url.title"
-	ConfigServerURLDescription = "plugin.cas_connector.backend.config.server_url.description"
+	InfoName                     = "plugin.cas_connector.backend.info.name"
+	InfoDescription              = "plugin.cas_connector.backend.info.description"
+	ConfigServerURLTitle         = "plugin.cas_connector.backend.config.server_url.title"
+	ConfigServerURLDescription   = "plugin.cas_connector.backend.config.server_url.description"
+	ConfigDisplayNameTitle       = "plugin.cas_connector.backend.config.display_name.title"
+	ConfigDisplayNameDescription = "plugin.cas_connector.backend.config.display_name.description"
 )
